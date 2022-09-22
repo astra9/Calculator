c1ff38b78ce71ffd831609b61cd698be3dcba99f
astra9 <thedigitalwhiterabbit@gmail.com>
2022-09-22 09:01:17 -0700

    changed NODE-IP

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
 
-NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
 NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
-./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
+./gradlew acceptanceTest -Dcalculator.url=http://192.168.59.102:${NODE_PORT}
